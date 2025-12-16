674be0cfd5fb0dcf149671e36027b90677a0a094
Dqnial <danial_20000@mail.ru>
2025-12-16 22:10:41 +0500

    change port to 5000

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import examRoutes from "./routes/exam.routes.js";
 
 const app = express();
 const __dirname = path.resolve();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
